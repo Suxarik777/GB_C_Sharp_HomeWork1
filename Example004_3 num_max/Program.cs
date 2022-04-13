442c77e3c7269f03c6c18c82224eed7a66d13ecf
Suxarik777 <gultcev@mail.ru>
2022-04-13 23:12:55 +0300

    Example004 Добавил задачу №4

---
+++ Example004_3 num_max/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите три числа и мы покажем какое число из них больше");
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int nummax = num1;
+
+if(num2 > nummax) nummax = num2;
+if(num3 > nummax) nummax = num3;
+
+Console.WriteLine("Наибольшее число " + nummax);
