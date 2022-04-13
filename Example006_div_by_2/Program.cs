781002056be00eef3d96ccf5c0992aa668ab820a
Suxarik777 <gultcev@mail.ru>
2022-04-13 23:39:14 +0300

    Example006 Добавил задачу №6

---
+++ Example006_div_by_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число и мы проверим является ли оно четным");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " является четным");
+}
+else
+{
+    Console.WriteLine("Число " + number + " нечетное число");
+}
