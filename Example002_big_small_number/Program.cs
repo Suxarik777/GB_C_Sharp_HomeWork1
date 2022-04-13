4665985b85d32c3573eff65221328854d763f011
Suxarik777 <gultcev@mail.ru>
2022-04-13 22:46:13 +0300

    Example002 Добавил задачу №2

---
+++ Example002_big_small_number/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два числа и мы покажем какое число больше, а какое меньше");
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("Больше первое число " + num1);
+    Console.WriteLine("А меньше второе число " + num2);
+}
+else
+{
+    Console.WriteLine("Первое число меньше " + num1);
+    Console.WriteLine("А второе число больше " + num2);
+}
+Console.WriteLine("Спасибо!");
+Console.WriteLine("Для перезапуска программы введите команду dotnet run");
