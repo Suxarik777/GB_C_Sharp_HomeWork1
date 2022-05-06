54c46f3baaedb897e5fbefac0f355efc9ac41523
Suxarik777 <gultcev@mail.ru>
2022-05-06 04:00:00 +0300

    HomeWork004

---
+++ Seminar_HomeWork004/Example025_A_degree_B/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25 
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Пример:  3, 5 -> 243 (3⁵)        2, 4 -> 16
+
+
+Console.WriteLine("Введите два числа и мы возведём первое число в натуральную степень второго");
+Console.WriteLine("Введите первое число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = Exponentiation(numberA, numberB);
+
+Console.WriteLine($"A возведённое в число B = {result}");
+
+
+int Exponentiation(int A, int B) //функция для возведения числа A в степень B
+{
+    int help = A;               // вспомогательная переменная для корректного возведения в степень изначально введёной цифры
+    for(int i = 1; i < B; i++)
+    {
+        A = A * help;
+    }
+    return A;
+}
---
+++ Seminar_HomeWork004/Example029_WriteArray/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine($"Напишите 8 чисел и мы выведим их на экран. Это будут числа в массиве");
+
+int[] numbers = new int[8];
+for (int i = 0; i < numbers.Length; i++)
+{    
+Console.WriteLine($"Введите число {i + 1}: ");    
+numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+WriteArray(numbers);
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine();
+}
