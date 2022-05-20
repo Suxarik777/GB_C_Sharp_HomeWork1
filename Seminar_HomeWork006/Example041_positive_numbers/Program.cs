9dedb82a6246ee33b3b85864f2f8d516a511efde
Suxarik777 <gultcev@mail.ru>
2022-05-20 15:36:41 +0300

    HomeWork005

---
+++ Seminar_HomeWork006/Example041_positive_numbers/Program.cs
@@ -0,0 +1,58 @@
+﻿// SЗадача 41 
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример:  
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Укажите, сколько чисел вы хотите ввести");
+int userlength = Convert.ToInt32(Console.ReadLine());
+
+int[] numbersArray = GetArray(userlength);
+
+Console.WriteLine("Вы ввели следующие числа: ");
+PrintArray(numbersArray);
+
+Console.WriteLine("");
+
+int positiveNumbersArray = CheckPositiveNumbers(numbersArray);
+Console.WriteLine($"Кол-во введёных чисел больше 0: {positiveNumbersArray}");
+
+
+
+int[] GetArray(int length)
+{
+    int[] newArray = new int[length];
+    FillArrayUser(newArray);
+    return newArray;
+}
+
+void FillArrayUser(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число № " + (i + 1));
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array [i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CheckPositiveNumbers(int[] array)
+{
+    int countCheck = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            countCheck++;
+        }
+    }
+    return countCheck;
+}
---
+++ Seminar_HomeWork006/Example043_intersection_of_lines/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//
+// Пример:
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите по 2 точки, для двух прямых и мы покажем их пересечение");
+double b1 = InputNumber("Введите точку b1: ");
+double b2 = InputNumber("Введите точку b2: ");
+double k1 = InputNumber("Введите точку k1: ");
+double k2 = InputNumber("Введите точку k2: ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine("Точка пересечения по оси X: " + x);
+Console.WriteLine("Точка пересечения по оси Y: " + y);
+
+
+double InputNumber(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
